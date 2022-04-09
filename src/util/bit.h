9f6076887c13505be36b133a2b539881aba11365
Jiwoo Park <jiwoo_90@naver.com>
2022-04-09 16:14:51 +0900

    Use built-in byte swap function

--- src/util/bit.h
+++ src/util/bit.h
@@ -3,26 +3,48 @@
 
 #include <cstdint>
 
-#include "type_traits"
+#ifdef _MSC_VER
+#include <cstdlib>
+#endif
+
+#include "type_traits.h"
 
 namespace bit {
 
 namespace detail {
 
-    constexpr std::uint16_t byteswap_u16(std::uint16_t value) noexcept
+    std::uint16_t byteswap_u16(std::uint16_t value) noexcept
     {
+#ifdef _MSC_VER
+        return _byteswap_ushort(value);
+#elif defined(__GNUC__) || defined(__clang__)
+        return __builtin_bswap16(value);
+#else
         return static_cast<std::uint16_t>((value << 8) | (value >> 8));
+#endif
     }
 
-    constexpr std::uint32_t byteswap_u32(std::uint32_t value) noexcept
+    std::uint32_t byteswap_u32(std::uint32_t value) noexcept
     {
+#ifdef _MSC_VER
+        return _byteswap_ulong(value);
+#elif defined(__GNUC__) || defined(__clang__)
+        return __builtin_bswap32(value);
+#else
         return (value << 24) | ((value << 8) & 0x00FF'0000) | ((value >> 8) & 0x0000'FF00) | (value >> 24);
+#endif
     }
 
-    constexpr std::uint64_t byteswap_u64(std::uint64_t value) noexcept
+    std::uint64_t byteswap_u64(std::uint64_t value) noexcept
     {
+#ifdef _MSC_VER
+        return _byteswap_uint64(value);
+#elif defined(__GNUC__) || defined(__clang__)
+        return __builtin_bswap64(value);
+#else
         return (value << 56) | ((value << 40) & 0x00FF'0000'0000'0000) | ((value << 24) & 0x0000'FF00'0000'0000) | ((value << 8) & 0x0000'00FF'0000'0000)
             | ((value >> 8) & 0x0000'0000'FF00'0000) | ((value >> 24) & 0x0000'0000'00FF'0000) | ((value >> 40) & 0x0000'0000'0000'FF00) | (value >> 56);
+#endif
     }
 
 } // namespace detail
@@ -48,11 +70,11 @@ constexpr T byteswap(T value) noexcept
 }
 
 enum class endian {
-#ifdef _WIN32
+#ifdef _MSC_VER
     little = 0,
     big = 1,
     native = little
-#else
+#elif defined(__GNUC__) || defined(__clang__)
     little = __ORDER_LITTLE_ENDIAN__,
     big = __ORDER_BIG_ENDIAN__,
     native = __BYTE_ORDER__
